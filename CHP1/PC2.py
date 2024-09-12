f6d88c90bc78833e079231f860d96ce836a80421
hassaan02 <hassan.mazher5@gmail.com>
2024-09-12 22:58:46 +0500

    complete 2nd PS

--- CHP1/PC2.py
+++ CHP1/PC2.py
@@ -23,3 +23,31 @@
 
 # print(type(user_input))
 
+
+# 4. Use comparison operator to find out whether ‘a’ given variable a is greater than 
+# ‘b’ or not. Take a = 34 and b = 80
+
+# a = 34
+# b = 80
+# print(a>b)
+
+
+# 5. Write a python program to find an average of two numbers entered by the user.
+
+# user_input1 = int(input("Type 1st no: ",))
+# user_input2 = int(input("Type 2nd no: ",))
+
+# result = user_input1 + user_input2 % 2
+
+# print(result)
+
+
+#  Write a python program to calculate the square of a number entered by the user.
+
+# user_input = int(input("Type A Number: ",))
+
+# result = user_input * user_input
+
+# print(result)
+
+
