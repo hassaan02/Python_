1591a818fdf79f5c1ef3daae66c0e37ddfba4291
hassaan02 <hassan.mazher5@gmail.com>
2024-09-12 22:31:15 +0500

    initial commit

---
+++ CHP1/PC1.py
@@ -0,0 +1,45 @@
+# Write a program to print Twinkle twinkle little star poem in python.
+
+# print('''
+#       Twinkle, twinkle, little star,
+# How I wonder what you are!
+# Up above the world so high,
+# Like a diamond in the sky.
+
+# Twinkle, twinkle, little star,
+# How I wonder what you are!
+
+# When the blazing sun is gone,
+# When he nothing shines upon,
+# Then you show your little light,
+# Twinkle, twinkle, all the night.
+
+# Twinkle, twinkle, little star,
+# How I wonder what you are!
+
+# Then the traveler in the dark
+# Thanks you for your tiny spark;
+# He could not see which way to go,
+# If you did not twinkle so.''')
+
+
+# Install an external module and use it to perform an operation of your interest.  
+
+# import pyttsx3
+# engine = pyttsx3.init()
+# engine.say("hassaan bhai kese ho yaar bhai thik hun tum sunao")
+# engine.runAndWait()
+
+
+
+# Write a python program to print the contents of a directory using the os module. 
+# Search online for the function which does that.  
+
+# import os
+# # Specify the path, or use '.' for the current directory
+# path = '-'
+# # List all files and directories in the specified path
+# contents = os.listdir(path)
+# # Print the contents
+# for item in contents:
+#     print(item)
---
+++ CHP1/PC2.py
@@ -0,0 +1,18 @@
+# Write a python program to add two numbers. 
+
+# a = 5
+# b = 7
+
+# sum = a+b
+
+# print("The sum of a and b is:", sum)
+
+
+# 2. Write a python program to find remainder when a number is divided by z.
+
+# num = 444
+# z = 66
+
+# remainder = float( num % z)
+
+print("The answer is:", remainder) hhhhh
