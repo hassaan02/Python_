e2704d644fad6e5bc3b06e83aaf569ee2ff82657
hassaan02 <hassan.mazher5@gmail.com>
2024-09-13 18:41:35 +0500

    strings done

---
+++ Practice Sets/PC3.py
@@ -0,0 +1,25 @@
+# 1. Write a python program to display a user entered name followed by Good 
+# Afternoon using input () function.
+
+# user_input = str(input("Enter Your Name Please: ",))
+# print("Good Afternoon", user_input)
+
+
+# Write a program to fill in a letter template given below with name and date. 
+# letter = '''  
+# Dear <|Name|>, 
+# You are selected! 
+# <|Date|> 
+# ''' 
+
+# print('''Dear Hassaan
+#     you are Selected!
+#     12-Sep-24''')
+
+# print("Dear hassaan\nYou are selected\n12-sep-24")
+
+
+# Write a program to detect double space in a string.
+# print("I am a\t boy")
+
+
