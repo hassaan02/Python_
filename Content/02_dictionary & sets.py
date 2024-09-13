f004d57efbf293fc8465e776b7c8083b3e64bdf3
hassaan02 <hassan.mazher5@gmail.com>
2024-09-13 23:14:04 +0500

    set added

--- Content/02_dictionary & sets.py
+++ Content/02_dictionary & sets.py
@@ -1,20 +1,35 @@
-marks = {
-    "hassy" : 22,
-    "hassu" : 33,
-    "hassi" : 44,
-    "hassa" : 66,
-    "hassz" : 77,
-}
+# marks = {
+#     "hassy" : 22,
+#     "hassu" : 33,
+#     "hassi" : 44,
+#     "hassa" : 66,
+#     "hassz" : 77,
+# }
 
 # marks.update({"hassy" : 34 })
 # print(marks)
 
 # print(marks.values())
 
-# print(marks.keys())
+#print(marks.keys())
 
 # marks.items()
 # print(marks)
 
 # get = marks.get("hassa")
 # print(get)
+
+
+
+# s = set()          # dont use s={} as set because it will be empty dictionary 
+
+set = {1,2,3,4,5,6,7,8,9}
+
+# print(set)
+
+# set.add(33)
+# print(set)
+
+# set.clear()
+# print(set)
+
