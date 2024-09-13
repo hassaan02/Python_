5eefd9ff070431b193312e80cadccf5aeac5f54a
hassaan02 <hassan.mazher5@gmail.com>
2024-09-13 22:39:32 +0500

    dict added

---
+++ Content/02_dictionary & sets.py
@@ -0,0 +1,20 @@
+marks = {
+    "hassy" : 22,
+    "hassu" : 33,
+    "hassi" : 44,
+    "hassa" : 66,
+    "hassz" : 77,
+}
+
+# marks.update({"hassy" : 34 })
+# print(marks)
+
+# print(marks.values())
+
+# print(marks.keys())
+
+# marks.items()
+# print(marks)
+
+# get = marks.get("hassa")
+# print(get)
