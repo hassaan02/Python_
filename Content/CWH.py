559678b5c3c6c1c88cf2a532b00f508fffc013bf
hassaan02 <hassan.mazher5@gmail.com>
2024-09-13 20:13:44 +0500

    list and tuple done

---
+++ Content/CWH.py
@@ -0,0 +1,30 @@
+list = ["Apple", "Tomato", "Banana", "Avacado", "Gava", "Cherry"]   # list is mutable
+list2 = [8,3,5,7,1,2,4,6,9]
+# print(list)
+
+# list.append("onion")    # put list item at end 
+# print(list)
+
+# list.reverse()
+# print(list)
+
+# list.insert(3, "orange")    # this will add item at index 3
+# print(list)
+
+# list.pop(2)        # remove item at index 2
+# print(list)
+
+# list.remove("Banana") 
+# print(list)
+
+# list2.sort()      #sorted list
+# print(list2)
+
+
+tuple = (5,8,2,5,7,9,2,6)           # tuple is immutable
+# print(type(tuple))
+
+# result = tuple.count(9)
+# print(result)
+
+# print(tuple.index(3))
