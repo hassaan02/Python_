d5c86c2a81dc9a5086aa6fcebe293109d796c0bc
hassaan02 <hassan.mazher5@gmail.com>
2024-09-16 20:56:05 +0500

    q2 added

--- Practice Sets/PC6.py
+++ Practice Sets/PC6.py
@@ -17,3 +17,30 @@
 #     greatest = num4
 
 # print("The greatest number is: ", greatest)
+
+# ===============================================================================================================
+
+# 2. Write a program to find out whether a student has passed or failed if it requires a 
+# total of 40% and at least 33% in each subject to pass. Assume 3 subjects and 
+# take marks as an input from the user.
+
+# max_marks = 100
+
+# sub1 = float(input("Enter the Marks of Subject 1: "))
+# sub2 = float(input("Enter the Marks of Subject 2: "))
+# sub3 = float(input("Enter the Marks of Subject 3: "))
+
+# total_marks = sub1 + sub2 + sub3
+# percentage = (total_marks / (3 * max_marks)) * 100
+
+# if(sub1 >= 33 and sub2 >= 33 and sub3 >= 33):
+#     if(percentage >= 40):
+#         print("you are Passed!")
+#     else:
+#         print("You are Faied!")
+
+# else:
+#     print("you are failed because of getting less marks in subject")             
+
+# print(percentage,"%")     
+
