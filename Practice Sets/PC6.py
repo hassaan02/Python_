a4cceff310799ecdebc55f83d247da305e9a457e
hassaan02 <hassan.mazher5@gmail.com>
2024-09-16 19:25:01 +0500

    q1 added

---
+++ Practice Sets/PC6.py
@@ -0,0 +1,19 @@
+# 1. Write a program to find the greatest of four numbers entered by the user. 
+
+# num1 = float(input("Enter the first number: "))
+# num2 = float(input("Enter the second number: "))
+# num3 = float(input("Enter the third number: "))
+# num4 = float(input("Enter the fourth number: "))
+
+# greatest = num1
+
+# if(num2 > greatest):
+#     greatest = num2
+
+# elif(num3 > greatest):
+#     greatest = num3    
+
+# elif(num4 > greatest):
+#     greatest = num4
+
+# print("The greatest number is: ", greatest)
