9c1196aa5fb2ec0fd55530f667dabd19e86bfae9
hassaan02 <hassan.mazher5@gmail.com>
2024-09-16 21:30:16 +0500

    q4 added

--- Practice Sets/PC6.py
+++ Practice Sets/PC6.py
@@ -66,3 +66,18 @@
 
 # else:
 #     print("This comment doesnt contain SPAM")  
+
+# ===============================================================================================================
+
+# Write a program to find whether a given username contains less than 10 
+# characters or not. 
+
+# username = input("Enter the username: ")
+
+# if len(username) < 10:
+#     print("Username is Less than 10 character")
+
+# else:
+#     print("Username is Greater than 10 characters")
+
+# ===============================================================================================================
