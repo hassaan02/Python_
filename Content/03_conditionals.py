d570f9af8921ed8b6b8b56028025ba508ba543a9
hassaan02 <hassan.mazher5@gmail.com>
2024-09-16 19:11:28 +0500

    conditionals added

---
+++ Content/03_conditionals.py
@@ -0,0 +1,10 @@
+# user_input = int(input("Write the age of your's:"))
+
+# if(user_input >= 18):
+#     print("you are allowed to attend party ")
+
+# elif(user_input <= 0):
+#     print("Write appropriate age!")
+
+# else:
+#     print("You are not alowed to attend party. ")
