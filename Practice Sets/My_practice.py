44bc93a6f083d894cbab3ee079c583c4e0d0a55f
hassaan02 <hassan.mazher5@gmail.com>
2024-09-17 23:31:55 +0500

    doing practice

---
+++ Practice Sets/My_practice.py
@@ -0,0 +1,66 @@
+#   ========================== Variables and Data Types ================================
+
+# Exercise 1: Create three variables: an integer, a float, and a string. Print their types
+
+# num = 5
+# float = 56.5
+# str = "hassu"
+
+# print(type(num))
+# print(type(float))
+# print(type(str))
+
+
+# Exercise 2: Convert an integer to a float and vice versa. Print the results.
+
+# int_value = 42
+# float_value = 42.0
+
+# convert_float = float(int_value)
+# convert_int = int(float_value)
+
+# print(convert_float)
+# print(convert_int)
+
+
+# ==========================  Strings ====================
+
+# Exercise 1: Create a string and perform the following operations:
+#     Find its length.
+#     Convert it to uppercase.
+#     Extract a substring.
+#     Replace a word.
+
+# str = "mynameishassaan"
+
+# print(len(str))
+
+# capt = str.upper()
+# print(capt)
+
+# sub_string = str[2:6]
+# print(sub_string)
+
+# replace = str.replace("hassaan", "ali")
+# print(replace)
+
+
+# Exercise 2: Concatenate two strings and print the result.
+
+# str1 = "mynameishassaan"
+# str2 = "andiamagoodboy"
+
+# conct = str1 + str2
+# print(conct)
+
+
+# Exercise 3: Take user input as a string and check if it contains only digits.
+
+# user_input = input("Write your name: ")
+
+# if(user_input.isdigit()):
+#     print("Yes it contaains digit")
+
+# else:
+#     print("it doesnt contain digit")
+
