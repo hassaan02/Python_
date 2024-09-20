84b0839cfdbd239840739c4e12fe7eebc712f084
hassaan02 <hassan.mazher5@gmail.com>
2024-09-20 22:49:27 +0500

    q1,2,3 added

---
+++ Practice Sets/PC7.py
@@ -0,0 +1,41 @@
+# 1. Write a program to print multiplication table of a given number using for loop. 
+
+# n = int(input("Write a number to give its multiplication table : "))
+
+# for i in range(1,11):
+#     print(f"{n} X {i} = {n * i}")
+
+
+# 2. Write a program to greet all the person names stored in a list ‘l’ and which starts 
+# with S. 
+# l = ["Harry", "Soham", "Sachin", "Rahul"]
+
+# l = ["Harry", "Soham", "Sachin", "Rahul"]
+
+# for name in l:
+#     if(name.startswith("S")):
+#         print(f"Hello {name}")
+
+
+# 3. Attempt problem 1 using while loop.
+
+# n = int(input("Write a number to give its multiplication table : "))
+
+# i = 1
+# while(i < 11):
+#     print(f"{n} X {i} = {n * i}")
+#     i += 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
