2dfb73687e6f2c3906e6ce27f525db0b77761d48
hassaan02 <hassan.mazher5@gmail.com>
2024-09-20 20:55:31 +0500

    loops added

---
+++ Content/04_loops.py
@@ -0,0 +1,25 @@
+# i = 1
+
+# while(i <= 50):
+#     print(i)
+#     i += 1
+
+
+# list = [1,2,3,4,5,6,7,8,9,10]
+# while(list):
+#     print(list)
+#     break
+
+
+# list = [1,2,3,4,5,6,7,8,9]
+# for i in list:
+#     print(i)
+
+# tuple = (1,2,3,4,5,6,7,8,9,10)
+# for i in tuple:
+#     print(i)
+
+# str = "Hassaan"
+# for i in str:
+#     print(i)
+
